f15c392a3727a06f6433c2d012bdb8af46f9727f
stygigoth <nyxwilliams@protonmail.com>
2023-12-06 03:56:37 -0500

    d6

--- d6/main.pl
+++ d6/main.pl
@@ -3,4 +3,52 @@
 use strict;
 use warnings;
 
-print("Hello World\n");
+my @lines = ();
+while (<>) {
+  chomp;
+  push(@lines,$_);
+}
+
+
+sub part_one {
+  my @times = split(' ', $_[0]);
+  my @distances = split(' ', $_[1]);
+  my %races = ();
+  for (my $i = 0; $i <= $#times; $i++) {
+    $races{$times[$i]} = $distances[$i];
+  }
+
+  my $a = 1;
+  for(keys %races) {
+      my $sum = 0;
+      for my $i (0..$_) {
+        if (($_ - $i) * $i > $races{$_}) {
+          $sum += 1;
+        }
+      }
+      if ($sum > 0) {
+        $a *= $sum;
+      }
+  }
+
+  print "Part One: ${a}\n"
+}
+
+sub part_two {
+  my $time = $_[0];
+  $time =~ s/\s+//g;
+  my $distance = $_[1];
+  $distance =~ s/\s+//g;
+  
+  my $sum = 0;
+  for my $i (0..$time) {
+    if (($time - $i) * $i > $distance) {
+      $sum += 1;
+    }
+  }
+
+  print "Part Two: ${sum}\n"
+}
+
+part_one(@lines);
+part_two(@lines);
