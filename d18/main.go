7bc76d6eaef5eb7c22ea3ceb57c610c0642150a0
stygigoth <nyxwilliams@protonmail.com>
2023-12-18 08:14:48 -0500

    d18

--- d18/main.go
+++ d18/main.go
@@ -1,7 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+type point struct{x, y uint64}
+
+func abs(i uint64) uint64 {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+func shoelace(points []point) uint64 {
+	var sum uint64
+	sum = 0
+	p0 := points[len(points) - 1]
+	for i, p1 := range points {
+		if i != len(points) - 1 {
+			sum += p1.x * (points[i + 1].y - p0.y)
+		} else {
+			sum += p1.x * (points[0].y - p0.y)
+		}
+		p0 = p1
+	}
+	return abs(sum) / 2
+}
+
+func solve(partOne bool) {
+	data, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(strings.Trim(string(data), "\n"), "\n")
+	var points []point
+	var sum uint64
+	var x uint64
+	var y uint64
+	sum = 0
+	x = 1
+	y = 1
+	for _, line := range lines {
+		instructions := strings.Split(line, " ")
+		d := string(strings.Trim(instructions[2], "(#)")[5:6])
+		amount, _ := strconv.ParseUint(strings.Trim(instructions[2], "(#)")[:5], 16, 32)
+		if d == "0" {
+			d = "R"
+		} else if d == "1" {
+			d = "D"
+		} else if d == "2" {
+			d = "L"
+		} else {
+			d = "U"
+		}
+		if partOne {
+			d = instructions[0]
+			amount, _ = strconv.ParseUint(instructions[1], 10, 64)
+		}
+		sum += amount
+		if d == "R" {
+			x += amount
+		} else if d == "L" {
+			x -= amount
+		} else if d == "D" {
+			y += amount
+		} else {
+			y -= amount
+		}
+		points = append(points, point{x, y})
+	}
+
+	area := shoelace(points)
+	if partOne {
+		fmt.Print("Part one: ")
+	} else {
+		fmt.Print("Part two: ")
+	}
+	fmt.Println(area + 1 - (sum / 2) + sum)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	solve(true)
+	solve(false)
 }
